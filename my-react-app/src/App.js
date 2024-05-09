73060d593f99733f79d0bd534f62dc9121dff6c8
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-09 19:14:25 +0000

    first

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Router as Router, Routes, Route } from "react-router-dom";
+
 import Accordian from "./index.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Accordian />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </Router>
+     
+        <Accordian/>
+      
+      
     </div>
   );
 }

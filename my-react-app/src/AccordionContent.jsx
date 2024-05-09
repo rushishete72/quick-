f3e14b5db9088569fd0024ec650461317aad988a
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-09 10:34:30 +0000

    rr

--- my-react-app/src/AccordionContent.jsx
+++ my-react-app/src/AccordionContent.jsx
@@ -1,23 +1,14 @@
-// App.js
+import React from "react";
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//import './App.css';
-import Accordian from './index.jsx';
-
-function App() {
+const AccordionContent = ({ dataItem, isContentVisible }) => {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Accordian />
-          </Route>
-          {/* Add more routes as needed */}
-        </Switch>
-      </Router>
+    <div
+      className="accordion-content"
+      style={{ display: isContentVisible ? "block" : "none" }}
+    >
+      <p>{dataItem.answer}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default AccordionContent;

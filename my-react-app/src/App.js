8245f316cac7147972311c5439b9332ccb8ab792
rushishete72 <166739003+rushishete72@users.noreply.github.com>
2024-05-09 19:59:41 +0000

    first

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,7 +5,8 @@ import Accordian from "./index.jsx";
 function App() {
   return (
     <div className="App">
-     
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9375274844031863"
+     crossorigin="anonymous"></script>
         <Accordian/>
       
       
